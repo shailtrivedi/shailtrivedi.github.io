020585bdf10e4267f1e022c8877141f8778597d6
Shail Trivedi <33336214+shailtrivedi@users.noreply.github.com>
2020-07-14 23:15:20 -0700

    Add files via upload

---
+++ Assets/js/custom.js
@@ -0,0 +1,89 @@
+$(window).load(function () {
+
+    // preloader
+//    $('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website.
+//    $('body').delay(250).css({
+//        'overflow': 'visible'
+//    });
+
+
+    //localStorage.setItem('exitintent_show', 'true'); // Set the flag in localStorage
+    
+    //  isotope
+    var $container = $('.portfolio_container');
+    $container.isotope({
+        filter: '*',
+    });
+
+    $('.portfolio_filter a').click(function () {
+        $('.portfolio_filter .active').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 500,
+                animationEngine: "jquery"
+            }
+        });
+        return false;
+    });
+
+    // back to top
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function () {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
+
+    // input
+    $(".input-contact input, .textarea-contact textarea").focus(function () {
+        $(this).next("span").addClass("active");
+    });
+    $(".input-contact input, .textarea-contact textarea").blur(function () {
+        if ($(this).val() === "") {
+            $(this).next("span").removeClass("active");
+        }
+    });
+});
+
+
+// Exit intent
+function addEvent(obj, evt, fn) {
+  if (obj.addEventListener) {
+    obj.addEventListener(evt, fn, false);
+  } else if (obj.attachEvent) {
+    obj.attachEvent("on" + evt, fn);
+  }
+}
+
+// Exit intent trigger
+addEvent(document, 'mouseout', function(evt) {
+  if (evt.toElement === null && evt.relatedTarget === null && !localStorage.getItem('exitintent_show')) {
+    $.magnificPopup.open({
+      items: {
+        src: '#newsletter-form' //ID of inline element       
+      },
+      type: 'inline',
+      removalDelay: 500, //Delaying the removal in order to fit in the animation of the popup
+      mainClass: 'mfp-fade mfp-fade-side', //The actual animation
+    });
+    localStorage.setItem('exitintent_show', 'true'); // Set the flag in localStorage
+  }
+});
